88c7c350d09f853f6eedf18c815870fc856e4d4b
Schuch666 <underschuch@gmail.com>
2024-05-25 14:26:37 -0400

    updates on plot_rast following terra updates for latlon customisation

--- R/plot_rast.R
+++ R/plot_rast.R
@@ -92,6 +92,11 @@ plot_rast <- function(r,
   vet_lat <- seq(-180,180,by = int)
   vet_lat <- vet_lat[vet_lat > min_lat - int & vet_lat < max_lat + int]
 
+  if(latlon){
+    ax  <- latlon(r = r,int = int,e = e_o)
+    pax <- c(pax,ax)
+  }
+
   extra <- function(){
     if(grid){
       terra::lines(terra::graticule(lon = vet_lon,lat = vet_lat,
@@ -133,10 +138,105 @@ plot_rast <- function(r,
 
     arg <- list(at=at, labels=label)
 
-    p <- terra::plot(r_log, col = color, plg = c(plg,arg), pax = pax,axe = !latlon, grid = FALSE,fun = extra, ...)
-    if(latlon) .plot.latlon(x = p,proj = terra::crs(r,proj=TRUE),int = int,e = e_o)
+    terra::plot(r_log, col = color, plg = c(plg,arg), pax = pax,axe = T, grid = FALSE,fun = extra, ...)
   }else{
-    p <- terra::plot(r2, col = color, plg = plg, pax = pax,axe = !latlon, grid = FALSE, fun = extra, ...)
-    if(latlon) .plot.latlon(x = p,proj = terra::crs(r,proj=TRUE),int = int,e = e_o)
+    terra::plot(r, col = color, plg = plg, pax = pax,axe = T, grid = FALSE,fun = extra, ...)
   }
+}
+
+#' @import terra
+#'
+latlon <- function(r,int,e,tn = 100) {
+
+  proj <-  terra::crs(r,proj=TRUE)
+
+  # latitude
+  vet_lon <- seq(-80,80,by = int)
+  lab_lon <- c(paste0(seq(80,int,by=-int),"\u00baS"),'0',
+               paste0(seq(int,80,by=int),"\u00baN"))
+
+  usr <- ext(r)
+  tx  <- rep(unname(usr[1]), tn)
+  ty  <- seq(unname(usr[3]), unname(usr[4]), length.out=tn)
+
+  firstPoints  <- vect(cbind(x = tx, y = ty), type="points",crs = proj)
+  tt           <- project(firstPoints,"+proj=longlat +datum=WGS84 +no_defs")
+  axis_coords  <- crds(tt)
+  tfcn         <- custom_approxfun(axis_coords[,2], ty)
+
+  # longitude
+  vet_lat <- seq(-180,180,by = int)
+  lab_lat <- c(paste0(seq(180,int,by=-int),"\u00baW"),'0',
+               paste0(seq(int,180,by=int),"\u00baE"))
+
+  tx <- seq(unname(usr[1]), unname(usr[2]), length.out = tn)
+  ty <- rep(unname(usr[3]), tn)
+
+  firstPoints  <- vect(cbind(x = tx, y = ty), type="points",crs = proj)
+  tt           <- project(firstPoints,"+proj=longlat +datum=WGS84 +no_defs")
+  axis_coords  <- crds(tt)
+  tfcn2        <- custom_approxfun(axis_coords[,1], tx)
+
+  min_lon <- as.numeric(e[1])
+  max_lon <- as.numeric(e[2])
+  min_lat <- as.numeric(e[3])
+  max_lat <- as.numeric(e[4])
+
+  valid_lat <- tfcn2(vet_lat)
+  valid_lon <- tfcn(vet_lon)
+
+  valid_lon[ valid_lon >= max_lat ] = NA
+  valid_lon[ valid_lon <= min_lat ] = NA
+  valid_lat[ valid_lat >= max_lon ] = NA
+  valid_lat[ valid_lat <= min_lon ] = NA
+
+  yat  <- valid_lon
+  ylab <- lab_lon
+  xat  <- valid_lat
+  xlab <- lab_lat
+
+  return(list(side = 1:2,
+              xat=xat, xlabs=xlab,
+              yat=yat, ylabs=ylab))
+}
+
+custom_approxfun <- function(x, y, method = "linear", rule = 1) {
+  if (length(x) != length(y)) {
+    stop("x and y must have the same length")
+  }
+  if (any(diff(x) <= 0)) {
+    stop("x must be strictly increasing")
+  }
+
+  interpolate <- function(new_x) {
+    n <- length(x)
+    if (method != "linear") {
+      stop("Only linear interpolation is supported")
+    }
+
+    if (any(new_x < x[1]) || any(new_x > x[n])) {
+      if (rule == 1) {
+        new_x <- pmax(pmin(new_x, x[n]), x[1])
+      } else {
+        stop("new_x values out of bounds and rule != 1")
+      }
+    }
+
+    y_new <- numeric(length(new_x))
+
+    for (i in seq_along(new_x)) {
+      if (new_x[i] <= x[1]) {
+        y_new[i] <- y[1]
+      } else if (new_x[i] >= x[n]) {
+        y_new[i] <- y[n]
+      } else {
+        j <- which(x <= new_x[i])
+        j <- j[length(j)]
+        y_new[i] <- y[j] + (y[j + 1] - y[j]) * (new_x[i] - x[j]) / (x[j + 1] - x[j])
+      }
+    }
+    return(y_new)
+  }
+
+  return(interpolate)
 }
