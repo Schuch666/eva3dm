ffaa1fa4550db31c4e0a036f0cdf92f1ce0f4e4c
Schuch666 <underschuch@gmail.com>
2024-07-06 01:21:07 -0400

    mda8 fixed

--- R/mda8.R
+++ R/mda8.R
@@ -3,21 +3,33 @@
 #' @description function to calculate Ozone Maximum Daily 8-hr Average or 8-hr moving Average for a data.frame
 #'
 #' @param data data.frame with time column and variable columns to be processed
-#' @param time name of the time column (defoult is date)
+#' @param time name of the time column (defoult is date) in POSIXct
 #' @param var name of the columns to be calculated
+#' @param verbose display additional information
 #' @return data.frame
 #' @export
 #'
-mda8 <- function(data, time = 'date', var) {
+mda8 <- function(data, time = 'date', var, verbose = TRUE) {
+  if(verbose)
+    cat('calculating MDA8 ... \n')
 
   data[[time]] <- as.POSIXct(data[[time]])
 
   if(missing(var)){
     VARS  = names(data)[!names(data) %in% time]
   }
-  output <- data.frame(date = data$date,stringsAsFactors = F)
+  var           <- VARS[1]
+  data$roll_avg <- NA
+  for (i in seq_along(data[[var]])) {
+    start_time       <- data[[time]][i]
+    end_time         <- start_time + 8*3600
+    within_8hr       <- data[[time]] >= start_time & data[[time]] < end_time
+    data$roll_avg[i] <- mean(data[[var]][within_8hr], na.rm = TRUE)
+  }
+  data$date          <- as.Date(data[[time]])
+  output             <- aggregate(roll_avg ~ date, data = data, FUN = max, na.rm = TRUE)
 
-  for(var in VARS){
+  for(var in VARS[-1]){
     data$roll_avg <- NA
     for (i in seq_along(data[[var]])) {
       start_time       <- data[[time]][i]
@@ -25,7 +37,6 @@ mda8 <- function(data, time = 'date', var) {
       within_8hr       <- data[[time]] >= start_time & data[[time]] < end_time
       data$roll_avg[i] <- mean(data[[var]][within_8hr], na.rm = TRUE)
     }
-    # data$date          <- as.Date(data[[time]])
     daily_max          <- aggregate(roll_avg ~ date, data = data, FUN = max, na.rm = TRUE)
     output             <- cbind(output,var = daily_max[,2])
   }
