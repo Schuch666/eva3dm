6ae5ba0d7042e8fb28b5d97f94ba306218b4b814
Schuch666 <underschuch@gmail.com>
2024-07-24 16:54:29 -0400

    extention of q2rh and rh2q to data.frames with time and extra examples

--- R/q2rh.R
+++ R/q2rh.R
@@ -2,19 +2,75 @@
 #'
 #' @description function to convert absolute humidity to relative humidity.
 #'
-#' @param q vector of relative humidity (in Kg/Kg)
-#' @param t vector of temperature (in K)
-#' @param p pressure (in Pa)
+#' @param q vector (or data.frame) of relative humidity (in g/Kg)
+#' @param t vector (or data.frame) of temperature (in K)
+#' @param p vector (or data.frame) of pressure (in Pa)
 #'
 #' @note default values are from standard atmosphere (288.15 K / 101325 Pa)
+#' @note if rh and temp arguments are data.frame, both need to have the same number of lines and columns, first column (time column) will be ignored.
 #'
 #' @export
 #'
+#' @return values of a data.frame, units are %
+#'
 #' @examples
+#' # for a single value (or same length vectors)
 #' q2rh(q = 0.0002038, t = 302.45, p = 100800)
 #'
-
+#' # using all data.frames
+#' times <- seq(as.POSIXct('2024-01-01',tz = 'UTC'),
+#'              as.POSIXct('2024-01-02',tz = 'UTC'),
+#'              by = 'hour')[1:5]
+#' q2   <- data.frame(time = times, a = rep(0.0002038,5))
+#' temp <- data.frame(time = times, a = rep(   302.45,5))
+#' pres <- data.frame(time = times, a = rep(   100800,5))
+#' q2rh(q = q2, t = temp, p = pres)
+#'
+#' # using data.frame for q and t (p is cte.)
+#' q2rh(q = q2, t = temp, p = 100000)
+#'
+#' # using data.frame for q and p (t is cte.)
+#' q2rh(q = q2, t = 300, p = pres)
+#'
+#' # using data.frame only for q (p and t are cte.)
+#' q2rh(q = q2, t = 300, p = 100000)
+#'
 q2rh <- function(q, t = 288.15, p = 101325){
+
+  if('data.frame' %in% class(q) & 'data.frame' %in% class(t) & 'data.frame' %in% class(p)){
+    obs     <- q
+    obs[-1] <- NA
+    for(j in 2:ncol(obs)){
+      obs[,j] <- q2rh(q = q[,j],t = t[,j],p = p[,j])
+    }
+    return(obs)
+  }
+  if('data.frame' %in% class(q) & 'data.frame' %in% class(t)){
+    obs     <- q
+    obs[-1] <- NA
+    for(j in 2:ncol(obs)){
+      obs[,j] <- q2rh(q = q[,j],t = t[,j],p = p)
+    }
+    return(obs)
+  }
+  if('data.frame' %in% class(q) & 'data.frame' %in% class(p)){
+    obs     <- q
+    obs[-1] <- NA
+    for(j in 2:ncol(obs)){
+      obs[,j] <- q2rh(q = q[,j],t = t,p = p[,j])
+    }
+    return(obs)
+  }
+
+  if('data.frame' %in% class(q)){
+    obs     <- q
+    obs[-1] <- NA
+    for(j in 2:ncol(obs)){
+      obs[,j] <- q2rh(q = q[,j],t = t,p = p)
+    }
+    return(obs)
+  }
+
   t  <- t - 273.15
   e  <- q * p / 0.622
   es <- 610.8 * exp(17.3*t /(t + 237.3))
--- R/rh2q.R
+++ R/rh2q.R
@@ -1,19 +1,53 @@
 #' Convert relative humidity to absolute humidity
 #'
-#' @description function to convert humidity to absolute humidity using tetens formula, assuming standart atmosphere conditions.
+#' @description function to convert humidity to absolute humidity using Tetens formula, assuming standard atmosphere conditions.
 #'
-#' @param rh vector of relative humidity (in percentage)
-#' @param temp vector of temperature (in celcius)
+#' @param rh vector (or data.frame) of relative humidity (in percentage)
+#' @param temp vector (or data.frame) of temperature (in Celsius)
 #'
 #' @note default values are from standard atmosphere (288.15 K)
+#' @note if rh and temp arguments are data.frame, both need to have the same number of lines and columns, first column (time column) will be ignored.
+#'
+#' @return value of data.frame, units are g/g.
 #'
 #' @examples
-#' rh2q(99)
+#' # for a singfle value
+#' rh2q(rh = 99, temp = 25)
 #'
+#' # vector of rh values
+#' rh2q(rh = c(0,seq(1,100, by = 4)), temp = 25)
+#'
+#' # vector of values for rh and temp
+#' rh2q(rh = c(0,seq(1,100, by = 4)), temp = 10:35)
+#'
+#' # rh is data.frame and temp is a value
+#' times <- seq(as.POSIXct('2024-01-01',tz = 'UTC'),
+#'              as.POSIXct('2024-01-02',tz = 'UTC'),
+#'              by = 'hour')
+#' rh2q(rh   = data.frame(time = times, a = seq(1,100, by = 4)),temp = 25)
+#'
+#' # using both rh and temp are data.frames
+#' rh2q(rh   = data.frame(time = times, a = seq(1,100, by = 4)),
+#'      temp = data.frame(time = times, a = 11:35))
 #' @export
 #'
-
 rh2q <- function(rh, temp = 15) {
+  if('data.frame' %in% class(rh) & 'data.frame' %in% class(temp)){
+    obs     <- rh
+    obs[-1] <- NA
+    for(j in 2:ncol(rh)){
+      obs[,j] <- rh2q(rh = rh[,j],temp = temp[,j])
+    }
+    return(obs)
+  }
+  if('data.frame' %in% class(rh)){
+    obs     <- rh
+    obs[-1] <- NA
+    for(j in 2:ncol(rh)){
+      obs[,j] <- rh2q(rh = rh[,j],temp = temp)
+    }
+    return(obs)
+  }
   temp  = temp + 273.15
   rh    = rh / 100
   qair <- rh * 2.541e6 * exp(-5415.0 / temp) * 18/29
