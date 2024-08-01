503c2621702cde7fdb0ba5a295ccf32cc786668a
Schuch666 <underschuch@gmail.com>
2024-08-01 18:44:37 -0400

    atr updated

--- R/atr.R
+++ R/atr.R
@@ -5,7 +5,7 @@
 #' @param file file name
 #' @param var variable name, 0 to global and "?" to show options
 #' @param att attribute names (NA for get all attnames)
-#' @param action Print, write or get (return the value) an attribute
+#' @param action "na" (only print), "write" or "get" (return the value) of an attribute
 #' @param value value to write
 #' @param verbose display additional information
 #'
@@ -15,19 +15,19 @@
 #'
 #' @examples
 #' nc <- paste0(system.file("extdata",package="eva3dm"),'/wrfinput_d01')
-#' atr(nc,0, action = 'print')
-#' atr(nc,'Times', action = 'print')
-#' atr(nc,'XLAT', action = 'print')
-#' atr(nc,'XLONG', action = 'print')
+#' atr(nc,0)
+#' atr(nc,'Times')
+#' atr(nc,'XLAT')
+#' atr(nc,'XLONG')
 #'
-#' atr(nc,'XLONG','MemoryOrder', action = 'print')
-#' atr(nc,'XLONG','description', action = 'print')
-#' atr(nc,'XLONG','units', action = 'print')
-#' atr(nc,'XLONG','stagger', action = 'print')
-#' atr(nc,'XLONG','FieldType', action = 'print')
+#' atr(nc,'XLONG','MemoryOrder')
+#' atr(nc,'XLONG','description')
+#' atr(nc,'XLONG','units')
+#' atr(nc,'XLONG','stagger')
+#' atr(nc,'XLONG','FieldType')
 #'
 
-atr <- function(file = NA,var = '?', att = NA, action="get", value=NA, verbose=F){
+atr <- function(file = NA,var = '?', att = NA, action="na", value=NA, verbose=T){
 
   meta <- NULL
   on.exit(ncdf4::nc_close(meta))
@@ -37,12 +37,12 @@ atr <- function(file = NA,var = '?', att = NA, action="get", value=NA, verbose=F
     file <- file.choose()   # nocov
     cat(paste(file,"\n"))   # nocov
   }
-  if(!action %in% c("read","get"))
+  if(!action %in% c("na","get"))
     to_write <- T
   else
     to_write <- F
 
-  meta <- ncdf4::nc_open(filename = file, verbose = verbose, write = to_write)
+  meta <- ncdf4::nc_open(filename = file, write = to_write)
 
   if(var == "?"){
     name  <- menu(names(meta$var), title = "Choose the variable:")
@@ -50,31 +50,51 @@ atr <- function(file = NA,var = '?', att = NA, action="get", value=NA, verbose=F
     var   <- names(meta$var)[name]
   }
 
-  if(action == "print" | action == "get"){
+  if(action == "na" | action == "get"){
     if(is.na(att)){
-      ATR <- ncdf4::ncatt_get(meta,var,att,verbose=verbose)
-      if(var==0)
-        cat("global attributes:\n")
-      else
-        cat(paste0("variable ",var," attritutes:\n"))
-      cat(paste(names(ATR),sep = ","))
+      ATR <- ncdf4::ncatt_get(meta,var,att)
+      if(var==0){
+        if(verbose) cat("global attributes:\n")
+      }else{
+        if(verbose) cat(paste0("variable ",var," attritutes:\n"))
+      }
+      if(verbose){
+        if(is.null(names(ATR))){
+          cat("not found\n")
+        }
+        else{
+          cat(paste(names(ATR),sep = ","))
+        }
+      }
     }else{
-      ATR <- ncdf4::ncatt_get(meta,var,att,verbose=verbose)
+      ATR <- ncdf4::ncatt_get(meta,var,att)
       if(var == 0)
         var <- "global"
-      cat(paste0(var," attribute ",att,":\n"))
-      cat(paste0(ATR$value,"\n"))
+      if(verbose)  cat(paste0(var," attribute ",att,":\n"))
+      if(verbose){
+        if(is.null(ATR$value)){
+          cat("not found\n")
+        }else{
+          if(ATR$value == ""){
+            cat("empty\n")
+          }else{
+            cat(paste0(ATR$value,"\n"))
+          }
+        }
+      }
     }
     if(action == 'get') return(ATR$value)
   }else{
     if(is.na(value))
       stop("nothing to write") # nocov
     if(var == 0){
-      cat(paste0("writing \'",value,"\' on global attribute \'",att,"\'\nat file ",file,"\n"))
+      if(verbose)
+        cat(paste0("writing \'",value,"\' on global attribute \'",att,"\'\nat file ",file,"\n"))
     }else{
-      cat(paste0("writing \'",value,"\' on attribute \'",att," \' of ",var,"\nat file ",file,"\n"))
+      if(verbose)
+        cat(paste0("writing \'",value,"\' on attribute \'",att," \' of ",var,"\nat file ",file,"\n"))
     }
 
-    ncdf4::ncatt_put(meta,varid = var,attname = att,attval = value,verbose = verbose)
+    ncdf4::ncatt_put(meta,varid = var,attname = att,attval = value)
   }
 }
