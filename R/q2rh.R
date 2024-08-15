e0701700f38b9fb944bf42f2fd25c32855fdd7fc
Schuch666 <underschuch@gmail.com>
2024-08-15 14:00:40 -0400

    using Celcius instead of Kelvin

--- R/q2rh.R
+++ R/q2rh.R
@@ -3,7 +3,7 @@
 #' @description function to convert absolute humidity to relative humidity.
 #'
 #' @param q vector (or data.frame) of relative humidity (in g/Kg)
-#' @param t vector (or data.frame) of temperature (in K)
+#' @param t vector (or data.frame) of temperature (in Celcius)
 #' @param p vector (or data.frame) of pressure (in Pa)
 #'
 #' @note default values are from standard atmosphere (288.15 K / 101325 Pa)
@@ -15,14 +15,14 @@
 #'
 #' @examples
 #' # for a single value (or same length vectors)
-#' q2rh(q = 0.0002038, t = 302.45, p = 100800)
+#' q2rh(q = 0.0002038, t = 29.3, p = 100800)
 #'
 #' # using all data.frames
 #' times <- seq(as.POSIXct('2024-01-01',tz = 'UTC'),
 #'              as.POSIXct('2024-01-02',tz = 'UTC'),
 #'              by = 'hour')[1:5]
 #' q2   <- data.frame(time = times, a = rep(0.0002038,5))
-#' temp <- data.frame(time = times, a = rep(   302.45,5))
+#' temp <- data.frame(time = times, a = rep(     29.3,5))
 #' pres <- data.frame(time = times, a = rep(   100800,5))
 #' q2rh(q = q2, t = temp, p = pres)
 #'
@@ -30,12 +30,12 @@
 #' q2rh(q = q2, t = temp, p = 100000)
 #'
 #' # using data.frame for q and p (t is cte.)
-#' q2rh(q = q2, t = 300, p = pres)
+#' q2rh(q = q2, t = 26, p = pres)
 #'
 #' # using data.frame only for q (p and t are cte.)
-#' q2rh(q = q2, t = 300, p = 100000)
+#' q2rh(q = q2, t = 26, p = 100000)
 #'
-q2rh <- function(q, t = 288.15, p = 101325){
+q2rh <- function(q, t = 15, p = 101325){
 
   if('data.frame' %in% class(q) & 'data.frame' %in% class(t) & 'data.frame' %in% class(p)){
     obs     <- q
@@ -71,7 +71,7 @@ q2rh <- function(q, t = 288.15, p = 101325){
     return(obs)
   }
 
-  t  <- t - 273.15
+  # t  <- t - 273.15
   e  <- q * p / 0.622
   es <- 610.8 * exp(17.3*t /(t + 237.3))
   RH <- 100 * e / es
