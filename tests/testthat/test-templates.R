29c378667dc49521afe4ee45b62d6091476bd45d
Schuch666 <underschuch@gmail.com>
2024-10-24 17:44:21 -0400

    test-that update

--- tests/testthat/test-templates.R
+++ tests/testthat/test-templates.R
@@ -1,7 +1,13 @@
 test_that("template works", {
 
   f <- file.path(tempdir(),"POST")
-  template(root = f)
+
+  template(root = f, template = 'WRF')      # WRF post process
+  template(root = f, template = 'WRF-Chem') # WRF-Chem post process
+  # template(root = f, template = 'METAR')    # download METAR
+  # template(root = f, template = 'AQ')       # eval QA
+  # template(root = f, template = 'MET')      # eval met
+  # template(root = f, template = 'GPCP')     # eval sat rain
 
   expect_equal(length(f), 1)
 })
